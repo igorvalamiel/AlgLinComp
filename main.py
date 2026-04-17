1e5b5ba0571e6036df69c1b531618195444f35f3
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-17 12:00:03 -0300

    criando arquivo inicial

---
+++ main.py
@@ -0,0 +1,17 @@
+
+def linha(): print('='*20)
+
+def cria_matriz(n):
+    pass
+
+def vetor(n):
+    pass
+
+
+linha()
+print("Boas Vindas ao solucionador de Equações Lineares!\n")
+print("Para o bom funcionamento do programa, siga as seguintes regras:")
+print('1) Insira o tamanho N da matriz NxN;')
+print('2) Aparecerão N linhas de input, insira os valores de cada linha separando com um espaço;')
+print('3) Insira os valores do vetor em uma única linha, separando os números com um espaço.')
+linha()
