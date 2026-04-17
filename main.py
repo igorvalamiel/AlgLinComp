3e1343122cb3bcb050fb664b3dbbb78b9702fc33
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-17 12:25:31 -0300

    main basicamente pronto

--- main.py
+++ main.py
@@ -32,7 +32,29 @@ n = int(input("N = "))
 M = cria_matriz(n)
 V = cria_vetor(n)
 
+# escolhendo método de solução
+linha()
+print("Esse programa usa 4 tipos de métodos para solução de Equações Lineares:")
+print("a) Eliminação Gaussiana")
+print("b) Fatoração LU")
+print("c) Método de Jacobi")
+print("d) Método de Gauss-Seidl")
+metodo = str(input("\nEscolha o método [a, b, c, d]: ").lower())
+linha()
+
+if metodo == 'a':
+    print("Método escolhido: Eliminação Gaussiana")
+elif metodo == 'b':
+    print("Método escolhido: Fatoração LU")
+elif metodo == 'c':
+    print("Método escolhido: Método de Jacobi")
+elif metodo == 'd':
+    print("Método escolhido: Método de Gauss-Seidl")
+else:
+    raise ValueError
+
+
 # vendo andamento
-print(n)
-print(M)
-print(V)
+#print(n)
+#print(M)
+#print(V)
