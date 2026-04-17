5c3c1599783721e38b1974fd3057b199ab43d282
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-17 12:15:49 -0300

    update no codigo main

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-
-def linha(): print('='*20)
+def linha(): print('='*90)
 
 def cria_matriz(n):
-    pass
+    m = []
+    for i in range(n):
+        linha = [int(j) for j in input(f"Linha {i+1}: ").split()]
+        
+        if len(linha) != n: raise IndexError #trantando erro se a linha tiver mais q n itens
 
-def vetor(n):
-    pass
+        m.append(linha)
+    return m
 
+def cria_vetor(n):
+    v = [int(j) for j in input(f"Vetor: ").split()]
 
+    if len(v) != n: raise IndexError #tratando erro se o vetor tiver mais q n valores
+
+    return v
+
+# -----------------------------------------------------------------------------------------------------
+# introdução
 linha()
 print("Boas Vindas ao solucionador de Equações Lineares!\n")
 print("Para o bom funcionamento do programa, siga as seguintes regras:")
-print('1) Insira o tamanho N da matriz NxN;')
+print('1) Insira o valor inteiro N da matriz NxN;')
 print('2) Aparecerão N linhas de input, insira os valores de cada linha separando com um espaço;')
 print('3) Insira os valores do vetor em uma única linha, separando os números com um espaço.')
 linha()
+
+# pegando matriz e vetor
+n = int(input("N = "))
+M = cria_matriz(n)
+V = cria_vetor(n)
+
+# vendo andamento
+print(n)
+print(M)
+print(V)
