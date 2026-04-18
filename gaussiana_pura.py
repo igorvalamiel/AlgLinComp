68965ae1e11080704a02f6a3ca967d82a104e220
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-18 15:42:41 -0300

    update 3

--- gaussiana_pura.py
+++ gaussiana_pura.py
@@ -6,16 +6,28 @@ def cria_matriz(n, M):
 def cria_vetor(n, M):
     return Matriz(n, 1, M)
 
+def back_solving(n, M, V):
+    x = [0]*n
+    m = M.mat.copy()
+    v = V.mat.copy()
+    for j in range(n-1, -1, -1):
+      x[j] = (m[j][j])/(v[j][0])
+      for i in range(j):
+          m[j][i] *= x[j]
+          v[j][0] += m[j][i]
+
+    return x
+
 def gaussiana_pura(n, M, V):
-    mat = cria_matriz(n, M)
+    matr = cria_matriz(n, M)
     vec = cria_vetor(n, V)
 
     for i in range(0, n-1):
         new_mat = Matriz(n, n, identity=True)
         for j in range(i, n):
-            v = mat.mat[i][i]
-            new_mat.mat[j][i] = float((-1*(mat.mat[j][i]))/v)
-        mat = new_mat * mat
+            v = matr.mat[i][i]
+            new_mat.mat[j][i] = float((-1*(matr.mat[j][i]))/v)
+        matr = new_mat * matr
         vec = new_mat * vec
-        print(mat, vec)
-        print('~~~~')
+
+    print(f"Soluções: {back_solving(n, matr, vec)}")
