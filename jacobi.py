e179d8b36e747cfa2f39e50f23d4edf7f56ad2b2
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-20 20:59:46 -0300

    testando recursão

--- jacobi.py
+++ jacobi.py
@@ -1,31 +1,31 @@
 from estruturas import Matriz
 
-def jacobi(n, m, v):
+def jacobizinho(n, m, v, x0, iter):
+    xi = [0 for _ in range(n)]
 
-    # contando as iterações e o erro
+    for i in range(n):
+        bi = v[i][0]
+        ai = m[i][i]
+        soma = 0
+        for j in range(n):
+            if i != j : soma += (m[i][j] * x0[i])
+        xi[i] = (bi-soma)/ai
+    
+    vec_x0 = Matriz(n, 1, x0)
+    vec_xi = Matriz(n, 1, xi)
+    subt_vec = vec_xi - vec_x0
+    R = subt_vec.module() / vec_xi.module()
+
+    return [iter+1, x0, xi, R]
+
+def jacobi(N, M, V):
+
+    # contando as iterações
     iterations = 0
-    R = 1
     
     # Vetor inicial
-    x0 = [1]*n
-
-    while R >= 0.00001:
-        xi = [0 for _ in range(n)]
-
-        for i in range(n):
-            bi = v[i][0]
-            ai = m[i][i]
-            soma = 0
-            for j in range(n):
-                if i != j : soma += (m[i][j] * x0[i])
-            xi[i] = (bi-soma)/ai
-        
-        vec_x0 = Matriz(n, 1, x0)
-        vec_xi = Matriz(n, 1, xi)
-        subt_vec = vec_xi - vec_x0
-        R = subt_vec.module() / vec_xi.module()
-        iterations += 1
-
-        print(iterations, x0, xi, R)
-
-        x0 = xi[:]
+    x0 = [1]*N
+    
+    res = jacobizinho(N, M, V, x0, iterations)
+    
+    print(res)
