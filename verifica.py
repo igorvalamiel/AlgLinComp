403ae0496eba0984372daf89850f35701df89011
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-20 13:59:58 -0300

    ajustando prova real para cálculos imprecisos

--- verifica.py
+++ verifica.py
@@ -1,8 +1,16 @@
+def is_different(x, y):
+    dif = x-y
+    if dif < 0: dif *= -1
+    print(dif)
+    if dif < 0.00000001: return False
+    return True
+
+
 def verifica(n, matr, vec, xList):
     for h in range(n):
         linha = matr.mat[h]
         s = 0
         for i in range(n):
             s += linha[i]*xList[i]
-        if s != vec.mat[h][0]: return f"O resultado está errado. Erro encontrado na linha {h} ~> ({s} != {vec.mat[h][0]})."
+        if is_different(s, vec.mat[h][0]): return f"O resultado está errado. Erro encontrado na linha {h} ~> ({s} != {vec.mat[h][0]})."
     return "O resultado está correto!"
