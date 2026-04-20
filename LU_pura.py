c6faa7ba9e61d016675042a5d14bc1cebcef7a0e
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-20 12:38:16 -0300

    FINALMENTE front_solving ta OK

--- LU_pura.py
+++ LU_pura.py
@@ -6,11 +6,32 @@ def cria_matriz(n, M):
 def cria_vetor(n, M):
     return Matriz(n, 1, M)
 
+def back_solving(n, M, V):
+    x = [0]*n
+    m = M.mat[:]
+    v = V.mat[:]
+    for j in range(n-1, -1, -1):
+        x[j] = (v[j][0])/(m[j][j])
+        for i in range(j):
+            m[i][j] *= x[j]
+            v[i][0] -= m[i][j]
+    return x
+
+def front_solving(n, M, V): # nome merda mas é a vida
+    y = Matriz(n, 1, [0]*n)
+    m = M.mat[:]
+    v = V.mat[:]
+    for j in range(n):
+        y.mat[j][0] = v[j][0]
+        for i in range(j+1, n):
+            m[i][j] *= y.mat[j][0]
+            v[i][0] -= m[i][j]
+    print(m, )
+    return y
+
 def LU_pura(n, M, V):
     matr = cria_matriz(n, M)
     vec = cria_vetor(n, V)
-
-    print(matr, '\n')
 
     # derivando matriz em LU (usando algoritmo do slide do prof)
     # não é necessário criar mais de uma matriz, já que L e U ocupam espaços diferentes
@@ -25,5 +46,7 @@ def LU_pura(n, M, V):
             for x in range(i+1, n):
                 matr.mat[x][k] -= matr.mat[x][i] * matr.mat[i][k]
     
-    print(matr) 
+    print(matr, '\n')
+    L_solved = front_solving(n, matr, vec)
+    print(L_solved)
         
