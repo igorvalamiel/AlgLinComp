1cb5b904173aa4789e577052478278e61ca74129
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-20 21:15:56 -0300

    jacobi OK

--- jacobi.py
+++ jacobi.py
@@ -1,6 +1,6 @@
 from estruturas import Matriz
 
-def jacobizinho(n, m, v, x0, iter):
+def jacobizinho(n, m, v, x0, iter, r_min, it_max):
     xi = [0 for _ in range(n)]
 
     for i in range(n):
@@ -8,7 +8,7 @@ def jacobizinho(n, m, v, x0, iter):
         ai = m[i][i]
         soma = 0
         for j in range(n):
-            if i != j : soma += (m[i][j] * x0[i])
+            if i != j : soma += (m[i][j] * x0[j])
         xi[i] = (bi-soma)/ai
     
     vec_x0 = Matriz(n, 1, x0)
@@ -16,13 +16,11 @@ def jacobizinho(n, m, v, x0, iter):
     subt_vec = vec_xi - vec_x0
     R = subt_vec.module() / vec_xi.module()
 
-    if R > 0.00001: jacobizinho(n, m, v, xi, iter+1)
-
     print(iter+1, x0, xi, R)
-    
-    return [iter+1, x0, xi, R]
 
-def jacobi(N, M, V):
+    if R > r_min and iter < it_max: return jacobizinho(n, m, v, xi, iter+1, r_min, it_max)
+
+def jacobi(N, M, V, R_min=0.0001, iter_max=50):
 
     # contando as iterações
     iterations = 0
@@ -30,5 +28,5 @@ def jacobi(N, M, V):
     # Vetor inicial
     x0 = [1]*N
     
-    res = jacobizinho(N, M, V, x0, iterations)
+    res = jacobizinho(N, M, V, x0, iterations, R_min, iter_max)
 
